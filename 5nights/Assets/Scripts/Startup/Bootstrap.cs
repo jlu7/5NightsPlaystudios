10bb5f11cd02734053b5794058f98790030adb39
jlu7 <tontonrue@gmail.com>
2016-10-27 15:31:26 -0700

    Bootstrap and ViewController created

---
+++ 5nights/Assets/Scripts/Startup/Bootstrap.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Bootstrap : MonoBehaviour
+{
+    public GameObject ViewAnchorRef;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(Startup());
+    }
+
+    IEnumerator Startup()
+    {
+        //Initiate The Singletons
+        //Transaction<List<TcgCard>> t = new Transaction<List<TcgCard>>();
+        yield return null;
+        ViewController.GetInstance().Initialize(ViewAnchorRef.transform);
+    }
+}
